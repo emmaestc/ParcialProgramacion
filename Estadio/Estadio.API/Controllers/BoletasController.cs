91a4ad43b73581c4565cfd3a05c56f2ac2557f0d
emmaestc <emmanuelestrada898@gmail.com>
2023-04-29 01:00:35 -0500

    Edit boleta

---
+++ Estadio/Estadio.API/Controllers/BoletasController.cs
@@ -0,0 +1,67 @@
+﻿using Estadio.API.Data;
+using Estadio.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Estadio.API.Controllers
+{
+    [ApiController]
+    [Route("/api/boletas")]
+    public class BoletasController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+
+        public BoletasController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAsync()
+        {
+            return Ok(await _context.Boletas.ToListAsync());
+        }
+
+        [HttpGet("{id:int}")]  //buscar por id
+        public async Task<ActionResult> GetAsync(int id)
+        {
+            var boleta = await _context.Boletas.FirstOrDefaultAsync(x => x.Id == id);
+            if (boleta == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(boleta);
+        }
+
+        [HttpPost]  //ingresar boletas
+        public async Task<ActionResult> PostAsync(Boleta boleta)
+        {
+            _context.Add(boleta);
+            await _context.SaveChangesAsync();
+            return Ok(boleta);
+        }
+
+        [HttpPut]  
+        public async Task<ActionResult> PutAsync(Boleta boleta)
+        {
+            _context.Update(boleta);
+            await _context.SaveChangesAsync();
+            return Ok(boleta);
+        }
+
+
+
+
+
+
+    }
+}
+
+
+
+    
+
+
+
